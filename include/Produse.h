9ae44294a02985b1e7d2ec33c5701825f4fcdb7e
Alexandra Oprea <opreaalexandra2005@gmail.com>
2026-01-06 02:34:50 +0200

    Rezolvarea finala

--- include/Produse.h
+++ include/Produse.h
@@ -45,7 +45,7 @@ protected:
 public:
     Skincare(const std::string& nume, float pret, const std::string& ten);
     void afisare(std::ostream& os) const override;
-    const std::string getTipTen() const { return  tipTen; }
+    const std::string& getTipTen() const { return  tipTen; }
 };
 
 class Crema : public Skincare
